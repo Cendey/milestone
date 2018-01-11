13111b0515865edab797b7f41fdf488d4705c909
Cendey <cendey@yeah.net>
2018-01-11 17:58:28 +0800

    General revise some tool utility.

---
+++ src/com/mit/lab/meta/Case.java
@@ -0,0 +1,44 @@
+package com.mit.lab.meta;
+
+import com.mit.lab.coms.Result;
+import com.mit.lab.intf.Supplier;
+
+/**
+ * <p>Title: Blueprint</p>
+ * <p>Description: com.mit.lab.meta.Case</p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <p>Company: Kewill Co., Ltd</p>
+ *
+ * @author <chao.deng@kewill.com>
+ * @version 1.0
+ * @date 01/11/2018
+ */
+public class Case<T> extends Tuple<Supplier<Boolean>, Supplier<Result<T>>> {
+
+    private Case(Supplier<Boolean> booleanSupplier, Supplier<Result<T>> resultSupplier) {
+        super(booleanSupplier, resultSupplier);
+    }
+
+    public static <T> Case<T> mcase(Supplier<Boolean> condition, Supplier<Result<T>> value) {
+        return new Case<>(condition, value);
+    }
+
+    public static <T> DefaultCase<T> mcase(Supplier<Result<T>> value) {
+        return new DefaultCase<>(() -> true, value);
+    }
+
+    private static class DefaultCase<T> extends Case<T> {
+
+        private DefaultCase(Supplier<Boolean> booleanSupplier, Supplier<Result<T>> resultSupplier) {
+            super(booleanSupplier, resultSupplier);
+        }
+    }
+
+    @SafeVarargs
+    public static <T> Result<T> match(DefaultCase<T> defaultCase, Case<T>... matches) {
+        for (Case<T> aCase : matches) {
+            if (aCase._1.get()) return aCase._2.get();
+        }
+        return defaultCase._2.get();
+    }
+}
