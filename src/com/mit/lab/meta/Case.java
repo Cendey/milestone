3b4731da826a71770cb802f7cf851174b148d586
Cendey <cendey@yeah.net>
2018-01-12 10:20:43 +0800

    General revise some tool utility.

--- src/com/mit/lab/meta/Case.java
+++ src/com/mit/lab/meta/Case.java
@@ -19,19 +19,19 @@ public class Case<T> extends Tuple<Supplier<Boolean>, Supplier<Result<T>>> {
         super(booleanSupplier, resultSupplier);
     }
 
+    private static class DefaultCase<T> extends Case<T> {
+
+        private DefaultCase(Supplier<Boolean> booleanSupplier, Supplier<Result<T>> resultSupplier) {
+            super(booleanSupplier, resultSupplier);
+        }
+    }
+
     public static <T> Case<T> mcase(Supplier<Boolean> condition, Supplier<Result<T>> value) {
         return new Case<>(condition, value);
     }
 
     public static <T> DefaultCase<T> mcase(Supplier<Result<T>> value) {
         return new DefaultCase<>(() -> true, value);
-    }
-
-    private static class DefaultCase<T> extends Case<T> {
-
-        private DefaultCase(Supplier<Boolean> booleanSupplier, Supplier<Result<T>> resultSupplier) {
-            super(booleanSupplier, resultSupplier);
-        }
     }
 
     @SafeVarargs
